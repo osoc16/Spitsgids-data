70075d1f0b10c12dd0f8f16a4e420588329cbfcb
Stan Callewaert <callewaert.stan@gmail.com>
2016-07-27 01:06:41 +0200

    Changed the php scripts to the latest database structure changes.

--- startscript.php
+++ startscript.php
@@ -3,7 +3,7 @@
 include_once 'vendor/autoload.php';
 use MongoDB\Collection as Collection;
 
-$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+$dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 $mongodb_url = getenv('MONGODB_URL');
 $mongodb_db = getenv('MONGODB_DB');
@@ -27,36 +27,16 @@ for($i=0; $i<2; $i++) {
     $structuralData = $structural->find(array('weekday' => $isWeekday));
 
     foreach ($structuralData as $structuralElement) {
-        $date = "";
         $extra = $i;
-        $time = $structuralElement->time;
 
-        if ($time >= 1000) {
-            $time = $time;
-        } elseif ($time >= 100) {
-            if($time < 400) {
-                $extra = $extra + 1;
-            }
-
-            $time = "0" . $time;
-        } elseif ($time >= 10) {
-            $extra = $extra + 1;
-            $time = "00" . $time;
-        } else {
-            $extra = $extra + 1;
-            $time = "000" . $time;
-        }
-
-        $date = date('Ymd\T', strtotime(date() . ' + ' . $extra . ' days'));
-        $time = $date . $time;
-        $connectionid = 'http://irail.be/connections/'.substr(basename($structuralElement->from), 2)."/".$date."/".$structuralElement->vehicle;
+        $date = date('Ymd', strtotime(date() . ' + ' . $extra . ' days'));
+        $connection = 'http://irail.be/connections/'.substr(basename($structuralElement->from), 2)."/".$date."/".$structuralElement->vehicle;
 
         $structuralToOccupancy = array(
-            'connection' => $connectionid,
-            'vehicle' => $structuralElement->vehicle,
+            'connection' => $connection,
+            'vehicle' => 'http://irail.be/vehicle/' . $structuralElement->vehicle,
             'from' => $structuralElement->from,
             'date' => $date,
-            'time' => $time,
             'structural' => $structuralElement->occupancy,
             'occupancy' => $structuralElement->occupancy
         );
